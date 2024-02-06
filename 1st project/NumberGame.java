9141049a4d53a11bc5b0a8426955376f84308b6c
Nomthandazo45 <122325571+Nomthandazo45@users.noreply.github.com>
2024-02-06 10:52:00 +0200

    Update NumberGame.java

--- 1st project/NumberGame.java
+++ 1st project/NumberGame.java
@@ -1,66 +1,46 @@
 import java.util.Random;
 import java.util.Scanner;
 
-Random random = new Random();
-int randomNumber = random.nextInt(100) + 1;
+public class NumberGame {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int score = 0;
+        
+        boolean playAgain = true;
+        
 
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter your guess: ");
-int userGuess = scanner.nextInt();
+        while (playAgain) {
+            int randomNumber = random.nextInt(100) + 1;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
 
-if (userGuess == randomNumber) {
-    System.out.println("Congratulations! Your guess is correct.");
-} else if (userGuess < randomNumber) {
-    System.out.println("Too low. Try again.");
-} else {
-    System.out.println("Too high. Try again.");
-}
+            while (!guessedCorrectly && attempts < 5) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
 
-boolean guessedCorrectly = false;
-int attempts = 0;
+                if (userGuess == randomNumber) {
+                    System.out.println("Congratulations! Your guess is correct.");
+                    guessedCorrectly = true;
+                    score++;
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Too low. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
+            }
+            System.out.println("Number of attempts: " + attempts);
 
-while (!guessedCorrectly) {
-    System.out.print("Enter your guess: ");
-    int userGuess = scanner.nextInt();
-    attempts++;
 
-    if (userGuess == randomNumber) {
-        guessedCorrectly = true;
-        System.out.println("Congratulations! Your guess is correct.");
-    } else if (userGuess < randomNumber) {
-        System.out.println("Too low. Try again.");
-    } else {
-        System.out.println("Too high. Try again.");
+            System.out.print("Play again? (yes/no): ");
+            String playAgainInput = scanner.next();
+            playAgain = playAgainInput.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Final score: " + score);
+
     }
 }
-
-int maxAttempts = 5;
-int attempts = 0;
-
-while (!guessedCorrectly && attempts < maxAttempts) {
-    // ...
-}
-
-if (attempts == maxAttempts) {
-    System.out.println("Sorry, you've reached the maximum number of attempts.");
-}
-
-boolean playAgain = true;
-
-while (playAgain) {
-    // Generate a new random number
-    // Reset the attempts counter
-
-    // Play the game
-
-    System.out.print("Do you want to play again? (yes/no): ");
-    String playAgainInput = scanner.next();
-
-    if (playAgainInput.equalsIgnoreCase("no")) {
-        playAgain = false;
-    }
-}
-
-int score = 0;
--
 

2aefc990e420136c32356d357d225a423b01d5f7
Nomthandazo45 <122325571+Nomthandazo45@users.noreply.github.com>
2024-02-07 14:40:38 +0200

    Update StudentGradeCalculator.java

--- 2nd project/StudentGradeCalculator.java
+++ 2nd project/StudentGradeCalculator.java
@@ -14,7 +14,8 @@ public class StudentGradeCalculator {
         int chemistryMarks = scanner.nextInt();
 
         scanner.close();
-
+        
+        // setting up values
         int totalMarks = biologyMarks + geographyMarks + chemistryMarks + ...;
         int numberOfSubjects = 3; // Assuming there are 3 subjects
         double averagePercentage = (double) totalMarks / numberOfSubjects;
